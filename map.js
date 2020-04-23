0649278eeac4857cdbb56e998f460c451185ddcc
GMCentroGeo <58823818+GMCentroGeo@users.noreply.github.com>
2020-04-22 20:32:26 -0500

    Update map.js

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 ///// Mapas base
 
-var osm = L.tileLayer('http://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
+var osm = L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
 {attribution: 'Map Data &copy; OpenstreetMap contributors'}); 
 
 var terrain = new L.StamenTileLayer("terrain");
@@ -11,12 +11,12 @@ var map = L.map('map',{
     layers:[terrain]
 });
 
-var googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
+var googleTerrain = L.tileLayer('https://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
     maxZoom: 20,
     subdomains:['mt0','mt1','mt2','mt3']
 }).addTo (map);
 
-var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+var googleSat = L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
     maxZoom: 20,
     subdomains:['mt0','mt1','mt2','mt3']
 }).addTo (map);
